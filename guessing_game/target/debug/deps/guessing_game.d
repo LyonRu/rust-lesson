8987fbae8bfd73a1ffcd38844ab4455dacaaee0b
LyonRu <lyonru29@gmail.com>
2021-01-21 16:06:16 +0800

    remove files

--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-/Users/lyonru/Work/rust-lesson/guessing_game/target/debug/deps/guessing_game: src/main.rs
-
-/Users/lyonru/Work/rust-lesson/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/lyonru/Work/rust-lesson/guessing_game/target/debug/guessing_game: /Users/lyonru/Work/rust-lesson/guessing_game/src/main.rs
